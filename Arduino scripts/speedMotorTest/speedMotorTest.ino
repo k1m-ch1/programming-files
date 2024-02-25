1429e3a638c4dd1c3aea647c550c93d61cb44164
k1m-ch1 <jingluy123@gmail.com>
2024-02-25 16:47:58 +0700

    motor speed control

---
+++ Arduino scripts/speedMotorTest/speedMotorTest.ino
@@ -0,0 +1,25 @@
+unsigned long long prevTime;
+unsigned int count = 0;
+
+void increaseCount(){
+  count++;
+  Serial.println("increased");
+}
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  pinMode(3, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(2), increaseCount, RISING);
+  prevTime = millis();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (millis() - prevTime > 1000){
+//    Serial.print("Count in 1 second ");
+//    Serial.println(count);
+    prevTime = millis();
+    count = 0;
+  }
+  
+}
