812cfbcda223644e926a47495af3e4777504daee
k1m-ch1 <jingluy123@gmail.com>
2025-12-16 01:33:50 +0700

    fetching spotify recommendations

---
+++ programming_projects/javascript/misc/fetching_songs.js
@@ -0,0 +1,66 @@
+async function checkSongArtist(songInput, artistInput) {
+  let response = await fetch('https://soundslikethis.us/api/checkSongArtist', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      song: songInput,
+      artist: artistInput
+    })
+  })
+  return response.json();
+}
+
+
+async function getFeatures(songId) {
+  let response = await fetch('https://soundslikethis.us/api/getFeatures', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      songId: songId
+    })
+  });
+  return response.json();
+}
+async function getRec(songId, songFeatures, num) {
+  const response = await fetch('https://soundslikethis.us/api/getRecs', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      songId1: songId,
+      features: songFeatures,
+      orig: songFeatures,
+      num: num
+    })
+  });
+  return response.json();
+}
+
+async function getRecDelta(origFeatures, recFeatures) {
+  let response = await fetch('https://soundslikethis.us/api/getRecDelta', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      origFeatures: origFeatures, //make this smoother
+      recFeatures: recFeatures
+    })
+  });
+  return response.json();
+}
+
+let data = await checkSongArtist("Fuck Time", "Green Day")
+let songId = data.tracks.items[0].id;
+console.log(songId);
+let features = await getFeatures(songId);
+console.log(features);
+let recDelta = await getRecDelta(features, features);
+console.log(recDelta);
+let rec = await getRec(songId, features, 10);
+console.log(rec);
